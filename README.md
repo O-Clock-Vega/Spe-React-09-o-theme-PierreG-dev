23d99e61578d9979b6ee88b577e490f88a7f6e6d
PierreG-dev <aniteck82@hotmail.fr>
2023-07-21 14:17:35 +0200

    fini darkmode

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Spe-React-09-o-theme-PierreG-dev
+# O'theme
+
+On a vu comment faire des composants afin de découper notre UI ! 💪
+
+On va pouvoir aller plus loin maintenant en venant introduire le concept de programmation déclarative grâce à la mise en place d'une source de vérité : **le state**
+
+Pour ça, on va se faire une petite application avec une seule feature : **le dark mode** 🤩
+
+## Initialisation du projet
+
+- Pour commencer on va cloner ce repo sur notre machine, puis se rendre dedans via le terminal
+- Ensuite, pour initialiser projet React rapidement on a vu un outils : **Create React App** !
+
+```sh
+# Pour init un projet React dans le dossier courant
+npx create-react-app .
+```
+
+:warning: Une fois la commande lancée, un nouveau fichier `README.md` sera crée. Celui ci sera renommé en `README.old.md`
+
+## Intégration basique
+
+Dans son état initial, l'application doit ressembler à quelque chose comme ça :
+
+!["Résultat light mode"](./docs/resultat1.png)
+
+Le logo est fournit dans le dossier `docs`
+
+## Dark mode
+
+Pour la feature il va falloir apprendre de nouvelles choses ! 😍
+
+- On va devoir apprendre à gérer les events dans une application React
+- Respecter le paradigme de la **programmation déclarative** avec la mise en place d'une sourcé de vérité ✨
+- Sa baser sur cette source de vérité afin d'afficher ce qu'il faut à l'utilisateur
+
+!["Résultat light mode"](./docs/resultat2.png)
+
+### LET'S GO ! :rocket:
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,71 @@
-import logo from '../../assets/logo.svg';
-
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
 
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
+    this.state = {
+      darkMode: false,
+    };
+  }
 
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.darkMode !== this.state.darkMode) {
+      console.log(
+        'darkMode a changé de',
+        prevState.darkMode,
+        'à',
+        this.state.darkMode
+      );
+    }
+  }
+
+  // --- Fonction qui switch le darkMode
+  toggleDarkMode = () => {
+    this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+  };
+
+  /**
+   * 
+   * // useEffect []
+  // Le composant vient de se créer
+  componentDidMount(): void {
+    console.log('toto');
+  }
+
+  // useEffect [darkMode(ex)]
+  // le composant vient de re-render
+  componentDidUpdate(
+    prevProps: Readonly<any>,
+    prevState: Readonly<any>
+  ): void {}
+
+  // useEffect [], () => {
+  //  return () => {
+  //
+  // }
+  // }
+  // lorsque le composant est détruit => exécute la fonction
+  componentWillUnmount(): void {}
+   */
+
+  render() {
+    return (
+      <main className={this.state.darkMode ? 'dark' : ''}>
+        <h1>O'light</h1>
+        <img
+          src={`/docs/${this.state.darkMode ? 'logo-green' : 'logo-gray'}.png`}
+          alt="React"
+        />
+        <button type="button" onClick={this.toggleDarkMode}>
+          Switch to dark mode
+        </button>
+      </main>
+    );
+  }
 }
-
 export default App;
